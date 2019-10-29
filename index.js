cd29d52336495c68322f0267b70e9fba4c15d7cb
Funmi7 <talabifunmilayo@gmail.com>
2019-10-29 14:12:11 +0100

    Added a POST and GET endponts

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // implement your API here
+const express = require('express')
+const cors = require('cors')
+const db = require('./data/db')
+
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            success: false,
+            err,
+        });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+
+    db.insert(userInfo)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            success: false,
+            err,
+        });
+    });
+});
+
+server.get('*', handleDefaultRequest)
+
+function handleDefaultRequest(req, res) {
+    res.json('hello world!')
+}
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('listening on ' + (process.env.PORT || 3000));
+})
+
